ebfd12bdf80d3b190223893fae9e3c50c7ca693e
diredocks <26994007+diredocks@users.noreply.github.com>
2025-01-02 13:41:37 +0800

    style: handler next line

--- internal/service/comment/handler.go
+++ internal/service/comment/handler.go
@@ -7,22 +7,25 @@ import (
 )
 
 func Index(c *fiber.Ctx) error {
-	return c.Status(fiber.StatusTeapot).SendString("I'm not a Tea pot!")
+	return c.Status(fiber.StatusTeapot).
+		SendString("I'm not a Tea pot!")
 }
 
 func Add(c *fiber.Ctx) error {
 	com := c.Locals("validatedBody").(*service.Comment)
 
 	if err := InsertDB(com); err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "failed to send comment",
-			"msg":   err.Error(),
-		})
+		return c.Status(fiber.StatusInternalServerError).
+			JSON(fiber.Map{
+				"error": "failed to send comment",
+				"msg":   err.Error(),
+			})
 	}
 
-	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
-		"success": "comment sent",
-	})
+	return c.Status(fiber.StatusCreated).
+		JSON(fiber.Map{
+			"success": "comment sent",
+		})
 }
 
 func Del(c *fiber.Ctx) error {
@@ -30,10 +33,11 @@ func Del(c *fiber.Ctx) error {
 	affected, err := DelDB(q)
 
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "failed to delete comments",
-			"msg":   err.Error(),
-		})
+		return c.Status(fiber.StatusInternalServerError).
+			JSON(fiber.Map{
+				"error": "failed to delete comments",
+				"msg":   err.Error(),
+			})
 	}
 
 	return c.JSON(fiber.Map{
--- internal/service/page/handler.go
+++ internal/service/page/handler.go
@@ -11,10 +11,11 @@ func Index(c *fiber.Ctx) error {
 	res, err := GetAllDB(&service.Page{})
 
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "failed to get pages",
-			"msg":   err.Error(),
-		})
+		return c.Status(fiber.StatusInternalServerError).
+			JSON(fiber.Map{
+				"error": "failed to get pages",
+				"msg":   err.Error(),
+			})
 	}
 
 	return c.JSON(res)
@@ -24,16 +25,18 @@ func Add(c *fiber.Ctx) error {
 	p := c.Locals("validatedBody").(*service.Page)
 
 	if err := InsertDB(p); err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "failed to add page",
-			"msg":   err.Error(),
-		})
+		return c.Status(fiber.StatusInternalServerError).
+			JSON(fiber.Map{
+				"error": "failed to add page",
+				"msg":   err.Error(),
+			})
 	}
 
-	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
-		"success": "page added",
-		"id":      p.ID,
-	})
+	return c.Status(fiber.StatusCreated).
+		JSON(fiber.Map{
+			"success": "page added",
+			"id":      p.ID,
+		})
 }
 
 func Get(c *fiber.Ctx) error {
@@ -41,10 +44,11 @@ func Get(c *fiber.Ctx) error {
 	res, err := QueryDB(p)
 
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "failed to get page",
-			"msg":   err.Error(),
-		})
+		return c.Status(fiber.StatusInternalServerError).
+			JSON(fiber.Map{
+				"error": "failed to get page",
+				"msg":   err.Error(),
+			})
 	}
 
 	return c.JSON(res)
@@ -54,10 +58,11 @@ func Del(c *fiber.Ctx) error {
 	p := c.Locals("validatedBody").(*service.Page)
 
 	if err := DelDB(p); err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "failed to delete page",
-			"msg":   err.Error(),
-		})
+		return c.Status(fiber.StatusInternalServerError).
+			JSON(fiber.Map{
+				"error": "failed to delete page",
+				"msg":   err.Error(),
+			})
 	}
 
 	return c.JSON(fiber.Map{
@@ -68,24 +73,27 @@ func Del(c *fiber.Ctx) error {
 func Update(c *fiber.Ctx) error {
 	data, err := FetchSitemap(config.Config("SITEMAP_URL"))
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "failed to fetch sitemap",
-			"msg":   err.Error(),
-		})
+		return c.Status(fiber.StatusInternalServerError).
+			JSON(fiber.Map{
+				"error": "failed to fetch sitemap",
+				"msg":   err.Error(),
+			})
 	}
 	pages, err := ParseSitemap(data)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "failed to parse sitemap",
-			"msg":   err.Error(),
-		})
+		return c.Status(fiber.StatusInternalServerError).
+			JSON(fiber.Map{
+				"error": "failed to parse sitemap",
+				"msg":   err.Error(),
+			})
 	}
 	affected, err := InsertsDB(pages)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "failed to insert sitemap into database",
-			"msg":   err.Error(),
-		})
+		return c.Status(fiber.StatusInternalServerError).
+			JSON(fiber.Map{
+				"error": "failed to insert sitemap into database",
+				"msg":   err.Error(),
+			})
 	}
 	return c.JSON(fiber.Map{
 		"success":  "updated sitemap",
