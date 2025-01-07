51d8ce33586405ced381cd618c0035b21f542cc5
diredocks <26994007+diredocks@users.noreply.github.com.>
2025-01-07 19:18:17 +0800

    feat: init fetch comment source

--- frontend/src/components/CommentBox.jsx
+++ frontend/src/components/CommentBox.jsx
@@ -1,19 +1,31 @@
-import { createSignal } from "solid-js";
+import { createSignal, onMount } from "solid-js";
 import CommentForm from "./CommentForm";
 import CommentList from "./CommentList";
 
+
+
 const CommentBox = () => {
 
-  const [comments, setComments] = createSignal([
-    { name: "Kytoki", content: "Hello there!", createdAt: "2025/1/4 12:34" },
-    { name: "Rynimo", content: "Nice to meet you!", createdAt: "2025/1/4 12:45" },
-    { name: "Genimis", content: "Great work!", createdAt: "2025/1/4 13:00" },
-    { name: "Terraock", content: "Keep it up!", createdAt: "2025/1/4 14:15" },
-  ])
+  const fetchComments = async () => {
+    const response = await fetch(`http://127.0.0.1:3001/api/page/get`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        site: "diredocks.github.io",
+        path: "/blog/",
+      }),
+    });
+    const data = await response.json();
+    setComments(data.Comments);
+  }
+
+  const [comments, setComments] = createSignal([]);
 
   const addComment = (comment) => {
     setComments([...comments(), comment]);
   }
+
+  onMount(fetchComments)
 
   return (
     <div>
